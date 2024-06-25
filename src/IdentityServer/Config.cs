a315fa7139342a1d3e0fb073c474a4c1f1b1e723
Amim7925 <amim.464432@gmail.com>
2024-06-25 17:38:48 +0330

    Add project files.

---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,36 @@
+﻿using Duende.IdentityServer.Models;
+using IdentityModel;
+
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        { 
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+            {
+                new ApiScope{ Name="basicinfo" , DisplayName = "Basic Information"},            
+                new ApiScope{ Name="newscms" , DisplayName = "News Content Management System"},            
+            };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[] 
+            { 
+                new Client
+                {
+                    ClientName = "newscmsClient",
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("newscmsClient".ToSha256())
+                    },
+
+                    AllowedGrantTypes = new List<string> { GrantType.ClientCredentials },
+                    AllowedScopes = { "basicinfo", "newscms" }
+                }
+            };
+}
