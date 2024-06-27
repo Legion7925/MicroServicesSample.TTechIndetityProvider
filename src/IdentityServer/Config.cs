e12c41037c7ad514307f7ae391d098bc4337b209
Amim7925 <amim.464432@gmail.com>
2024-06-27 17:26:58 +0330

    init

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 using IdentityModel;
 
 namespace IdentityServer;
@@ -7,30 +8,51 @@ public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
         new IdentityResource[]
-        { 
-            new IdentityResources.OpenId()
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
             {
-                new ApiScope{ Name="basicinfo" , DisplayName = "Basic Information"},            
-                new ApiScope{ Name="newscms" , DisplayName = "News Content Management System"},            
+                new ApiScope{ Name="basicinfo" , DisplayName = "Basic Information"},
+                new ApiScope{ Name="newscms" , DisplayName = "News Content Management System"},
             };
 
     public static IEnumerable<Client> Clients =>
-        new Client[] 
+        new Client[]
             { 
+                //new Client
+                //{
+                //    ClientName = "newscmsClient",
+                //    ClientId = "newscmsClient",
+                //    ClientSecrets = new List<Secret>
+                //    {
+                //        new Secret("newscmsClient".ToSha256())
+                //    },
+
+                //    AllowedGrantTypes = new List<string> { GrantType.ClientCredentials },
+                //    AllowedScopes = { "basicinfo", "newscms" }
+                //}
                 new Client
                 {
                     ClientName = "newscmsClient",
+                    ClientId = "newscmsClient",
                     ClientSecrets = new List<Secret>
                     {
                         new Secret("newscmsClient".ToSha256())
                     },
-
-                    AllowedGrantTypes = new List<string> { GrantType.ClientCredentials },
-                    AllowedScopes = { "basicinfo", "newscms" }
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = { "https://localhost:7105/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:7105/signout-callback-oidc" },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "basicinfo", "newscms"
+                    }
+                    ,AllowOfflineAccess = true,
                 }
             };
 }
--- src/IdentityServer/Program.cs
+++ src/IdentityServer/Program.cs
@@ -1,4 +1,8 @@
 ﻿using IdentityServer;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
@@ -10,6 +14,27 @@ Log.Information("Starting up");
 try
 {
     var builder = WebApplication.CreateBuilder(args);
+
+    const string serviceName = "IdentityServer.API";
+
+    builder.Logging.AddOpenTelemetry(options =>
+    {
+        options
+            .SetResourceBuilder(
+                ResourceBuilder.CreateDefault()
+                    .AddService(serviceName))
+            .AddConsoleExporter();
+    });
+    builder.Services.AddOpenTelemetry()
+          .ConfigureResource(resource => resource.AddService(serviceName))
+          .WithTracing(tracing => tracing
+              .AddAspNetCoreInstrumentation()
+              .AddConsoleExporter()
+              .AddJaegerExporter()
+              .AddSqlClientInstrumentation())
+          .WithMetrics(metrics => metrics
+              .AddAspNetCoreInstrumentation()
+              .AddConsoleExporter());
 
     builder.Host.UseSerilog((ctx, lc) => lc
         .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
